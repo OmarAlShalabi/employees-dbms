f5e2902fb42c3dd7c30e4f381090c21b693e8f1b
Omar AL-Shalabi <shalbooz@Shalbooz.lan>
2024-09-14 11:57:34 +0300

    Add Initial Schema.

---
+++ src/main/resources/db/migration/V20240914112601__initial_schema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE IF NOT EXISTS currency (
+    code VARCHAR(5) NOT NULL,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    decimals INT NOT NULL,
+    PRIMARY KEY(code)
+);
+
+CREATE TABLE IF NOT EXISTS employee (
+    id uuid,
+    fullName VARCHAR(200) NOT NULL,
+    email VARCHAR(256) NOT NULL,
+    position VARCHAR(200) NOT NULL,
+    salary DECIMAL NOT NULL,
+    currency_code VARCHAR(5) NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_currency_code
+        FOREIGN KEY(currency_code)
+            REFERENCES currency(code)
+);
+
+INSERT INTO
+    currency (code, name, decimals)
+VALUES
+    ('USD', 'US Dollar', 4);
