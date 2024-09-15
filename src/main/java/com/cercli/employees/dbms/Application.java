e4be8f9b62a65cc9c5dd635656c8a44762194a98
Omar AL-Shalabi <shalbooz@Shalbooz.lan>
2024-09-15 16:04:26 +0300

    Remove Extra RestController Annotation.

--- src/main/java/com/cercli/employees/dbms/Application.java
+++ src/main/java/com/cercli/employees/dbms/Application.java
@@ -6,13 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class Application {
-
-	@GetMapping("/")
-	public boolean test() {
-		return true;
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
