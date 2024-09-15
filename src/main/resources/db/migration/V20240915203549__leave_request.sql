60468a67229cbcbd96330f0961647440a637fd56
Omar AL-Shalabi <shalbooz@Shalbooz.lan>
2024-09-15 20:53:49 +0300

    Add Employee Id Relation To Time Request Migration.

--- src/main/resources/db/migration/V20240915203549__leave_request.sql
+++ src/main/resources/db/migration/V20240915203549__leave_request.sql
@@ -7,12 +7,16 @@ CREATE TABLE IF NOT EXISTS request_category (
 CREATE TABLE IF NOT EXISTS time_off_request (
     id INT GENERATED ALWAYS AS IDENTITY,
     request_category_id INT NOT NULL,
+    employee_id uuid NOT NULL,
     start_date TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     end_date TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(id),
     CONSTRAINT fk_request_category_id
         FOREIGN KEY(request_category_id)
-            REFERENCES request_category(id)
+            REFERENCES request_category(id),
+    CONSTRAINT fk_employee_id
+        FOREIGN KEY(employee_id)
+            REFERENCES employee(id)
 );
 
 INSERT INTO
